ca5061d23583b11c9f0eee95924803f6f21439f3
tchiteu <matheussantos.contato362@gmail.com>
2020-12-19 11:41:08 -0300

    Unifying css files

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div class="container">
       <Head>
-        <title>First Next App</title>
+        <title>Matheus Santos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <div className={styles.main_container}>
-        <header className={styles.header}>
+      <div class="main_container">
+        <header class="header">
           <a href="">Front-end</a>
 
           <a href="">Back-end</a>
@@ -20,19 +19,19 @@ export default function Home() {
           <a href="">UX</a>
         </header>
 
-        <section className={styles.content_container}>
+        <section class="content_container">
           <div style={{height: "900px"}}></div>
         </section>
       </div>
 
-      <section className={styles.avatar_container}>
-        <div className={styles.avatar}>
+      <section class="avatar_container">
+        <div class="avatar">
 
         </div>
 
         <h2>Matheus Santos</h2>
 
-        <div className={styles.personal_info}>
+        <div class="personal_info">
 
         </div>
       </section>
